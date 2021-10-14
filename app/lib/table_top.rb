50b79c04460647747f6b13e12d56762840de24a4
Cyril Ologho <cyrielo911@gmail.com>
2021-10-15 00:23:28 +0100

    update tabletop implementation

--- app/lib/table_top.rb
+++ app/lib/table_top.rb
@@ -1,12 +1,14 @@
-class TableTop
-  def initialize(length = 5, breadth = 5)
-    @left_boundary = @bottom_boundary = 0
-    @right_boundary, @top_boundary = length - 1, breadth - 1
-  end
+module ToyRobot
+  class TableTop
+    attr_reader :left_boundary, :right_boundary, :top_boundary, :bottom_boundary
+    def initialize(length = 5, breadth = 5)
+      @left_boundary = @bottom_boundary = 0
+      @right_boundary, @top_boundary = length - 1, breadth - 1
+    end
 
-  def is_within_bounds?(x = 0, y = 0)
-    x.between?(@left_boundary, @right_boundary) &&
-      y.between?(@bottom_boundary, @top_boundary)
+    def is_within_bounds?(x = 0, y = 0)
+      x.between?(@left_boundary, @right_boundary) &&
+        y.between?(@bottom_boundary, @top_boundary)
+    end
   end
-
 end
