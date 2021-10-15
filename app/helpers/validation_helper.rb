3ba8b72b695ba874f5f0db24f0bae95ccae551bb
Cyril Ologho <cyrielo911@gmail.com>
2021-10-15 14:45:03 +0100

    handle edge case for validation_helper

--- app/helpers/validation_helper.rb
+++ app/helpers/validation_helper.rb
@@ -2,16 +2,17 @@ require_relative '../constants/application_constants'
 module ToyRobot
   class ValidationHelper
     def self.is_valid_cardinal?(cardinal)
-      cardinal = cardinal.downcase.to_sym if cardinal
+      cardinal = cardinal.downcase.to_sym if cardinal.is_a?(String)
       CARDINALS.has_key?(cardinal)
     end
 
     def self.is_valid_command?(command)
-      command = command.upcase if command
+      command = command.upcase if command.is_a?(String)
       VALID_COMMANDS.include?(command)
     end
 
     def self.is_valid_integer?(*nums)
+      return false if nums.nil?
       nums.each { |num| Integer(num) rescue return false }.any?
     end
 
