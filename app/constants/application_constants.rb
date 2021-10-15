dd5c61e20f75999dbf1ade00d644bc68648b2fbb
Cyril Ologho <cyrielo911@gmail.com>
2021-10-15 01:30:11 +0100

    add version number

--- app/constants/application_constants.rb
+++ app/constants/application_constants.rb
@@ -1,4 +1,5 @@
 module ToyRobot
+  VERSION = "1.0".freeze
   VALID_COMMANDS = %w(PLACE MOVE REPORT LEFT RIGHT).freeze
   CARDINALS = { south: :SOUTH, west: :WEST, north: :NORTH, east: :EAST }.freeze
 end
