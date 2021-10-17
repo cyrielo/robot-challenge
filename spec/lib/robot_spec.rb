baa39202ddc462d6ba5473b5faf9e7b3249e0563
Cyril Ologho <cyrielo911@gmail.com>
2021-10-17 03:00:27 +0100

    refactor position variable

--- spec/lib/robot_spec.rb
+++ spec/lib/robot_spec.rb
@@ -11,14 +11,14 @@ RSpec.describe ToyRobot::Robot do
     it 'prevents placing robot on table with invalid positioning' do
       expect(@robot.table_top).eql?(@table_top)
       @robot.place(5, 5, 'west')
-      expect(@robot.robot_position).to be_nil
+      expect(@robot.position).to be_nil
     end
 
     it 'should place robot with valid coordinates and cardinal direction' do
       @robot.place(4, 2, 'north')
-      expect(@robot.robot_position.cardinal_direction).to eql(:NORTH)
-      expect(@robot.robot_position.x_coord).to eql(4)
-      expect(@robot.robot_position.y_coord).to eql(2)
+      expect(@robot.position.cardinal_direction).to eql(:NORTH)
+      expect(@robot.position.x_coord).to eql(4)
+      expect(@robot.position.y_coord).to eql(2)
     end
   end
 
@@ -39,8 +39,8 @@ RSpec.describe ToyRobot::Robot do
       @robot.place(x, y, cardinal)
       @robot.move
       @robot.move
-      expect(@robot.robot_position.y_coord).to eql(y + 2)
-      expect(@robot.robot_position.x_coord).to eql(x)
+      expect(@robot.position.y_coord).to eql(y + 2)
+      expect(@robot.position.x_coord).to eql(x)
     end
 
     it 'it should not advance robot position if beyond table-top boundary' do
@@ -50,8 +50,8 @@ RSpec.describe ToyRobot::Robot do
       @robot.place(x, y, cardinal)
       @robot.move
       @robot.move
-      expect(@robot.robot_position.y_coord).to eql(y)
-      expect(@robot.robot_position.x_coord).to eql(x)
+      expect(@robot.position.y_coord).to eql(y)
+      expect(@robot.position.x_coord).to eql(x)
     end
   end
 
@@ -62,9 +62,9 @@ RSpec.describe ToyRobot::Robot do
       cardinal = 'east'
       @robot.place(x, y, cardinal)
       @robot.left
-      expect(@robot.robot_position.cardinal_direction).to eql(:NORTH)
-      expect(@robot.robot_position.y_coord).to eql(y)
-      expect(@robot.robot_position.x_coord).to eql(x)
+      expect(@robot.position.cardinal_direction).to eql(:NORTH)
+      expect(@robot.position.y_coord).to eql(y)
+      expect(@robot.position.x_coord).to eql(x)
     end
   end
 
@@ -75,9 +75,9 @@ RSpec.describe ToyRobot::Robot do
       cardinal = 'east'
       @robot.place(x, y, cardinal)
       @robot.right
-      expect(@robot.robot_position.cardinal_direction).to eql(:SOUTH)
-      expect(@robot.robot_position.y_coord).to eql(y)
-      expect(@robot.robot_position.x_coord).to eql(x)
+      expect(@robot.position.cardinal_direction).to eql(:SOUTH)
+      expect(@robot.position.y_coord).to eql(y)
+      expect(@robot.position.x_coord).to eql(x)
     end
   end
 end
