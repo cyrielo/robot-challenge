fd26656a5d0bbac3769b882192a123f2c7d48fa9
Cyril Ologho <cyrielo911@gmail.com>
2021-10-19 23:45:55 +0100

    fix spec

--- spec/constants/application_constants_spec.rb
+++ spec/constants/application_constants_spec.rb
@@ -5,14 +5,11 @@ RSpec.describe 'ToyRobot::ApplicationConstants' do
     it "ensures constants cannot be modified and are not empty" do
       expect(ToyRobot::VALID_COMMANDS).to be_a(Array)
       expect(ToyRobot::VALID_COMMANDS.frozen?).to eql(true)
-      expect(ToyRobot::VALID_COMMANDS.length).to eql(5)
+      expect(ToyRobot::VALID_COMMANDS.length).to eql(7)
 
       expect(ToyRobot::CARDINALS).to be_a(Hash)
       expect(ToyRobot::CARDINALS.frozen?).to eql(true)
       expect(ToyRobot::CARDINALS.keys.length).to eql(4)
-
-      expect(ToyRobot::VERSION.frozen?).to eql(true)
-      expect(ToyRobot::VERSION).to be_a(String)
     end
   end
 end
