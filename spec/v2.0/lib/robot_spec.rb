84d89edccfb3b935781744c5e17d0b66afc5e234
Cyril Ologho <cyrielo911@gmail.com>
2021-10-21 18:05:59 +0100

    add spec for robotv2

--- spec/v2.0/lib/robot_spec.rb
+++ spec/v2.0/lib/robot_spec.rb
@@ -6,5 +6,27 @@ RSpec.describe ToyRobot::Robot do
   let(:robot) { ToyRobot::Factory.create_robotv2 }
   subject { robot }
 
-  it_should_behave_like "a Robot"  
+  it_should_behave_like "a Robot"
+
+  context '#spawn' do
+    it 'should spawn robot at random a place' do
+      expect(subject.position.coords.any?).to be_falsy
+      expect(subject.report).to be_nil
+      subject.spawn
+      expect(subject.report).to be_a(String)
+      expect(subject.position.coords.any?).to be_truthy
+    end
+  end
+
+  context '#obstruct' do
+    it 'prevent robot from advancing to obstructed position' do
+      subject.place(0, 3, 'NORTH')
+      subject.obstruct
+      subject.move
+      subject.move
+      expect(subject.report).to eql('0,3 NORTH')
+      subject.place(0, 4, 'NORTH')
+      expect(subject.report).to eql('0,3 NORTH')
+    end
+  end
 end
