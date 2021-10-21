617b00012f847e0c3dfaebc271e64e063b54108f
Cyril Ologho <cyrielo911@gmail.com>
2021-10-21 19:18:36 +0100

    change version

--- bin/robot_cli.rb
+++ bin/robot_cli.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require_relative '../app/factory/robot_factory'
 
-application = ToyRobot::Factory.create_application_v2
+application = ToyRobot::Factory.create_application
 cli_app = ToyRobot::Factory.create_cli(app: application)
 print application.help_prompt
 cli_app.start
