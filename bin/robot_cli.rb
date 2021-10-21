f6d0bc20b5e254badcb30efc8365ed3baec3487e
Cyril Ologho <cyrielo911@gmail.com>
2021-10-21 05:11:10 +0100

    update cli program

--- bin/robot_cli.rb
+++ bin/robot_cli.rb
@@ -3,6 +3,9 @@ require_relative '../app/lib/cli.rb'
 require_relative '../app/factory/toy_robot'
 
 application = ToyRobot::Factory.create_application_v2
+reader = ToyRobot::Factory.create_reader
+writer = ToyRobot::Factory.create_writer
 
-ToyRobot::Cli.new(application: application)
+writer.writeln(application.help_prompt)
+ToyRobot::Cli.new(app: application, reader: reader, writer: writer)
 
