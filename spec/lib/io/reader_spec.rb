59f56988a1ff5433b933dfc138ea23830d004dd7
Cyril Ologho <cyrielo911@gmail.com>
2021-10-21 06:30:29 +0100

    add reader spec

--- spec/lib/io/reader_spec.rb
+++ spec/lib/io/reader_spec.rb
@@ -1,5 +1,4 @@
-require_relative '../../app/lib/io/reader'
-
+require_relative '../../../app/lib/io/reader'
 RSpec.describe ToyRobot::Reader do
 
   context 'behave like a file reader' do
@@ -8,18 +7,20 @@ RSpec.describe ToyRobot::Reader do
     end
 
     it 'should have lines' do
-      expect(@reader).to have_attributes(:lines)
+      expect(@reader).to have_attributes(:lines => nil)
     end
+
     it 'should have readline' do
-      expect(reader).to respond_to(:readline).with(1..2).arguments
+      expect(@reader).to respond_to(:readline).with(1).arguments
     end
   end
 
   context '#file' do
     it 'should actually readlines' do
-      path = '/Users/cyrielo/projects/robot-challenge/spec/test_data/oneline.txt';
-      @reader = reader.file(path)
-      expect(reader.lines).to eql('oneliner')
+      path = '/Users/cyrielo/projects/robot-challenge/spec/test_data/oneline.txt'
+      reader = ToyRobot::Reader.new
+      reader = reader.file(path)
+      expect(reader.lines.join('')).to eql('oneliner')
     end
   end
 end
