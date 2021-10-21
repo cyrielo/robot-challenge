c0c06aa38179d45671f71752b03fb1f532ee096d
Cyril Ologho <cyrielo911@gmail.com>
2021-10-21 19:21:26 +0100

    client for v2

---
+++ bin/robot_cli_v2.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+require_relative '../app/factory/robot_factory'
+
+application = ToyRobot::Factory.create_application_v2
+cli_app = ToyRobot::Factory.create_cli(app: application)
+print application.help_prompt
+cli_app.start
